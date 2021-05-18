02545bf76e6feca8b04a1efedd7a5f47d814e91f
Chisom Trisha Okoye <trisha.omabu@gmail.com>
2021-05-18 00:01:54 -0600

    Data asset added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import reactDom from "react-dom";
 
-function App() {
+function Header() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header>
+      <h1>Header</h1>
+    </header>
   );
+}
+
+function Main() {
+  return (
+    <main>
+      <h2>Main</h2>
+    </main>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>
+      <h2>Footer</h2>
+    </footer>
+  );
+}
+
+//https://api.github.com/users/trishachi
+
+function App({ login }) {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${login}`)
+      .then((response) => response.json())
+      .then(setData);
+  }, []);
+
+  if (data) {
+    return <div>{JSON.stringify(data)}</div>;
+  }
+  return <div>No User Found</div>;
+
+  // return (
+  //   <div className="App">
+  //     <Header />
+  //     <Main />
+  //     <Footer />
+  //   </div>
+  // );
 }
 
 export default App;
