d36aaba05e5fd8919a7e49bbde8024a7473207ab
Chisom Trisha Okoye <trisha.omabu@gmail.com>
2021-05-22 23:57:32 -0600

    Categories data setup

--- src/App.js
+++ src/App.js
@@ -1,46 +1,37 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./App.css";
-import reactDom from "react-dom";
+
 import Data from "./assets/data/data.json";
 
-function Header() {
-  return (
-    <header>
-      <h1>Header</h1>
-    </header>
-  );
-}
-
-function Main() {
-  return (
-    <main>
-      <p>Main</p>
-    </main>
-  );
-}
-
-function Footer() {
-  return (
-    <footer>
-      <h2>Footer</h2>
-    </footer>
-  );
-}
-
-//https://api.github.com/users/trishachi
-
 function App() {
-  const getData = () => {
-    for (var i = 0; i < Data.questions.length; i++) {
-      console.log(Data.questions.question);
-    }
-  };
+  function getData() {}
 
   return (
     <div className="App">
-      <Header />
-      <Main getData={this.getData} />
-      <Footer />
+      <header className="App-header">
+        <h1 className="App-title">Developer Interview Questions App</h1>
+        <h2>
+          Short description of how the app works and waht the appis all about ie
+          aims to achieve
+        </h2>
+        <button className="call-to-action">
+          Select a Category below to begin
+        </button>
+      </header>
+      <main>
+        <div>
+          {Data.categories.map((category) => {
+            // if (question.category.includes("javascript")) {
+            //   return <h4 key={question.question_id}>{question.question}</h4>;
+            return <button className="select-category">{category}</button>;
+          })}
+        </div>
+      </main>
+      <footer>
+        <p className="copyright font-italic">
+          <em>Copyright © 2020 | Chisom Trisha Okoye </em>
+        </p>
+      </footer>
     </div>
   );
 }
